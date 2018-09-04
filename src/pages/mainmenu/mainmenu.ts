7798b71bad6dfc478ae67e7393a7fbbb7554c080
jose.paz@roket.mx <ClickIT@MacBook-Pro-de-Moni.local>
2018-09-04 09:18:40 -0500

    remove comments

--- src/pages/mainmenu/mainmenu.ts
+++ src/pages/mainmenu/mainmenu.ts
@@ -41,34 +41,23 @@ export class MainmenuPage {
     this.balance = this.balance - Number(this.amountToWithdraw);
 
     var importe:string = this.amountToWithdraw.toString();
-    // indicamos todas las monedas posibles
-    // indicamos todas las monedas posibles
     var coins=[100, 50, 20, 10, 5];
     var manyCoins = [3,3,3,3,3];
-    
-    // creamos un array con la misma cantidad de monedas
-    // Este array contendra las monedas a devolver
+  
     var change=[0,0,0,0];
     
-    // Recorremos todas las monedas
     for(var i=0; i<coins.length; i++)
     {
-        // Si el importe actual, es superior a la moneda
         if(parseInt(importe)>=coins[i])
         {
-          // obtenemos cantidad de monedas
           change[i]=Math.floor( parseInt(importe)/coins[i]);
     
-          // actualizamos el valor del importe que nos queda por didivir
           importe=`${parseInt(importe)-(change[i]*coins[i])}`;
-
-          //Remove the coins
           manyCoins[i] = manyCoins[i] - change[i];
           
         }
     }
     
-    // Bucle to show result
     for(i=0; i<coins.length; i++)
     {
         if(change[i]>0)
